53350d92102b629a39f82daa09b99cb2076507ae
stormbroken <181250184@smail.nju.edu.cn>
2021-03-02 14:43:50 +0800

    update DIP

---
+++ src/main/java/principle/DIP/MainClass.java
@@ -0,0 +1,14 @@
+package principle.DIP;
+
+/**
+ * @Author stormbroken
+ * Create by 2021/03/02
+ * @Version 1.0
+ **/
+
+public class MainClass {
+    private AbstractSource abstractSource;
+    private AbstractTransformer abstractTransformer;
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/principle/DIP/TextSource.java
@@ -0,0 +1,10 @@
+package principle.DIP;
+
+/**
+ * @Author stormbroken
+ * Create by 2021/03/02
+ * @Version 1.0
+ **/
+
+public class TextSource implements AbstractSource{
+}
